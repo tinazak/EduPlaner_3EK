349c57a3dd968b6e46fd792df89352343a130a2a
Tina Zak <Tina.Zak@s.hakmistelbach.ac.at>
2026-05-18 12:44:46 +0200

    angelegt

---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HAK_BlazorPicoTemplate.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Geben Sie bitte einen Benutzername ein!")]
+        public string Username { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Geben Sie bitte ein Passwort ein!")]
+        [MinLength(6, ErrorMessage = "Das Passwort muss 6 Zeichen enthalten!")]
+        public string Password { get; set; } = string.Empty;
+    }
+}
